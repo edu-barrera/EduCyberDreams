4edec29a7f3e1ef50ea9cfc85d2bc2ed70c0d791
edu-barrera <barreracarloseduardo03@gmail.com>
2024-06-20 15:45:51 -0600

    added about section and started with recent projects section

--- Assets/main.js
+++ Assets/main.js
@@ -1,6 +1,10 @@
 const downloadBtn = document.getElementById("download-btn");
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const colorList = ["#F26B9C", "#F266C1", "#55CCD9", "#8C4C46", "#261515"];
+const welcomePort = document.querySelector(".welcome-port");
+const aboutTitle = document.querySelector(".about-title");
+const parentElement = document.querySelector(".parent-element");
+const aboutSection = document.createElement("div");
 
 document.addEventListener("DOMContentLoaded", (event) => {
   downloadBtn.innerText = downloadBtn.innerText
@@ -9,23 +13,105 @@ document.addEventListener("DOMContentLoaded", (event) => {
       return alphabet[Math.floor(Math.random() * 26)];
     })
     .join("");
-});
 
-downloadBtn.onmouseover = (event) => {
+  // Reverting to correct text
+  // Animation to revert to original text
   let iterations = 0;
   const interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    downloadBtn.innerText = downloadBtn.innerText
       .split("")
       .map((letter, index) => {
         if (index < iterations) {
-          return event.target.dataset.value[index];
+          return downloadBtn.dataset.value[index];
         }
-        return alphabet[Math.floor(Math.random() * 26)];
+        return alphabet[Math.floor(Math.random() * alphabet.length)];
       })
       .join("");
 
-    if (iterations >= event.target.dataset.value.lenght)
+    if (iterations >= downloadBtn.dataset.value.length) {
       clearInterval(interval);
-    iterations += 1 / 2;
+      downloadBtn.classList.add("download-cv");
+    }
+
+    iterations += 0.5;
   }, 50);
-};
+  const addGlitchEffect = () => {
+    downloadBtn.classList.add("glitch-effect");
+    setTimeout(() => {
+      downloadBtn.classList.remove("glitch-effect");
+    }, 1000); // Duration of the glitch effect
+  };
+
+  const triggerGlitchEffectRandomly = () => {
+    const randomDelay = Math.random() * 3000 + 2000; // Random delay between 2 to 7 seconds
+    setTimeout(() => {
+      addGlitchEffect();
+      triggerGlitchEffectRandomly();
+    }, randomDelay);
+  };
+
+  triggerGlitchEffectRandomly();
+});
+
+parentElement.addEventListener("click", function (e) {
+  if (e.target.classList.contains("about-btn-studies")) {
+    document.createElement("div");
+    aboutSection.classList.add("about-sections");
+    if (aboutSection) {
+      aboutSection.remove();
+    }
+    aboutSection.innerHTML = `
+      <div class="mensaje-incompleto">
+        <h2>Studies</h2>
+        <p> por favor completa la sección </p>
+      </div>`; // Mensaje al tratar de mandar form incompleto
+
+    aboutTitle.insertAdjacentElement("afterend", aboutSection);
+  }
+  if (e.target.classList.contains("about-btn-languages")) {
+    document.createElement("div");
+    aboutSection.classList.add("about-sections");
+    if (aboutSection) {
+      aboutSection.remove();
+    }
+    aboutSection.innerHTML = `
+      <div class="mensaje-incompleto">
+        <h2>Languages</h2>
+        <p> por favor completa la sección </p>
+      </div>`; // Mensaje al tratar de mandar form incompleto
+
+    aboutTitle.insertAdjacentElement("afterend", aboutSection);
+  }
+  if (e.target.classList.contains("about-btn-goals")) {
+    document.createElement("div");
+    aboutSection.classList.add("about-sections");
+    if (aboutSection) {
+      aboutSection.remove();
+    }
+    aboutSection.innerHTML = `
+      <div class="mensaje-incompleto">
+        <h2>Goals</h2>
+        <p> por favor completa la sección </p>
+      </div>`; // Mensaje al tratar de mandar form incompleto
+
+    aboutTitle.insertAdjacentElement("afterend", aboutSection);
+  }
+  if (e.target.classList.contains("about-btn-why")) {
+    document.createElement("div");
+    aboutSection.classList.add("about-sections");
+    if (aboutSection) {
+      aboutSection.remove();
+    }
+    aboutSection.innerHTML = `
+      <div class="mensaje-incompleto">
+        <h2>Why</h2>
+        <p> por favor completa la sección </p>
+      </div>`; // Mensaje al tratar de mandar form incompleto
+
+    aboutTitle.insertAdjacentElement("afterend", aboutSection);
+  }
+
+  if (e.target.classList.contains("about-btn-connect")) {
+    window.open("https://www.linkedin.com/in/barrera-carlos-e/", "_blank");
+  }
+});
